94b7e1b6a776cb304b8a2b1601c6a355ac51962b
Aakash <ak2247@cornell.edu>
2025-03-13 15:35:16 -0400

    Add spectrogram logic

--- nearpy/plots/__init__.py
+++ nearpy/plots/__init__.py
@@ -1,3 +1,4 @@
 from .evaluation import plot_pretty_confusion_matrix 
-from .timeseries import plot_routine_template, plot_time_series
+from .timedomain import plot_routine_template, plot_time_series
+from .freqdomain import plot_spectrogram
 from .constants import BBC_THEME, DEFAULT_PLOT_STYLE
