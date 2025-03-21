fe3f6e3b490306bdcf07bb3c2404948a4383e461
Aakash Kapoor <113218196+meowkash@users.noreply.github.com>
2025-03-21 18:46:06 -0400

    Fix spectrogram code

--- nearpy/preprocess/spectrum.py
+++ nearpy/preprocess/spectrum.py
@@ -5,17 +5,23 @@ import matplotlib.pyplot as plt
 from librosa.feature import melspectrogram
 from librosa.display import specshow
 
-def get_spectrogram(sig, fs, seg_frac=4.5, perc_overlap=0.5, visualize=False): 
+def get_spectrogram(sig, fs, seg_frac=20, perc_overlap=0.5, visualize=False): 
     # We use the same defaults that matlab uses for consistency across code-bases 
     N = len(sig) 
-    nperseg = np.floor(N/seg_frac) # Divide signal into segments of fixed length
-    noverlap = np.floor(perc_overlap*nperseg) # Define overlap between contiguous segments 
-    win = hamming(nperseg)
-    nfft = max(256, 2**np.log2(nperseg)) # Compute number of points to take FFT on 
+    nperseg = int(np.floor(N/seg_frac)) # Divide signal into segments of fixed length
+    noverlap = int(np.floor(perc_overlap*nperseg)) # Define overlap between contiguous segments 
+    win = hamming(int(nperseg))
+    nfft = max(256, nperseg) # Compute number of points to take FFT on 
     
     # Define STFT object to get STFT and Spectrogram
-    SFT = ShortTimeFFT.from_window(win, fs, nperseg, noverlap, fft_mode='centered',
-                               mfft=nfft, scale_to='magnitude', phase_shift=None)
+    SFT = ShortTimeFFT(win=win, 
+                       hop=noverlap, 
+                       fs=fs, 
+                       fft_mode='onesided',
+                       mfft=nfft, 
+                       scale_to='magnitude', 
+                       phase_shift=None)
+    
     specgram = SFT.spectrogram(sig)
 
     if visualize: 
@@ -28,15 +34,15 @@ def get_spectrogram(sig, fs, seg_frac=4.5, perc_overlap=0.5, visualize=False):
         
         Sx_dB = 10 * np.log10(np.fmax(specgram, 1e-4))  # limit range to -40 dB
         im = ax.imshow(Sx_dB, origin='lower', aspect='auto',
-                        extent=SFT.extent(N), cmap='turbo')
+                        extent=SFT.extent(N), cmap='jet')
         fig.colorbar(im, label='Power Spectral Density (dB)')
         
         # Shade areas where window slices stick out to the side:
         for t0_, t1_ in [(t_lo, SFT.lower_border_end[0] * SFT.T),
                         (SFT.upper_border_begin(N)[0] * SFT.T, t_hi)]:
-            ax.axvspan(t0_, t1_, color='w', linewidth=0, alpha=.3)
+            ax.axvspan(t0_, t1_, color='w', linewidth=0.1, alpha=.3)
         for t_ in [0, N * SFT.T]:  # mark signal borders with vertical line
-            ax.axvline(t_, color='c', linestyle='--', alpha=0.5)
+            ax.axvline(t_, color='y', linestyle='--', alpha=0.5)
         
         fig.tight_layout()
         plt.show()
