91937a1826a34f387a770cdd0deef913fa82edcb
Aakash <ak2247@cornell.edu>
2025-04-02 18:03:44 -0400

    Improve confusion matrix aesthetics

--- nearpy/plots/evaluation.py
+++ nearpy/plots/evaluation.py
@@ -5,7 +5,12 @@ import seaborn as sns
 
 from ..utils import get_accuracy
 
-def plot_pretty_confusion_matrix(cmat, gestures, cmap='Greens', sub_id=None, save=False, save_path=None):
+def plot_pretty_confusion_matrix(cmat, 
+                                 gestures, 
+                                 cmap=sns.light_palette("brown", as_cmap=True), 
+                                 sub_id=None, 
+                                 save=False, 
+                                 save_path=None):
     # Store overall confusion matrix over all subjects 
     cc = np.zeros((len(gestures), len(gestures)))
 
@@ -31,18 +36,23 @@ def plot_pretty_confusion_matrix(cmat, gestures, cmap='Greens', sub_id=None, sav
                 _plot_pretty_confusion_matrix(cm, gestures, plot_title, cmap, save, spath)
         
         # Plot overall confusion matrix
-        plot_title = 'Overall classification accuracy'
+        plot_title = 'Classification Accuracy'
         if save:
             spath = os.path.join(save_path, f'overall_confusion_matrix')
         _plot_pretty_confusion_matrix(cc, gestures, plot_title, cmap, save, spath)
 
-def _plot_pretty_confusion_matrix(cm, gestures, plot_title, cmap, save=False, save_path=None):
+def _plot_pretty_confusion_matrix(cm, 
+                                  gestures, 
+                                  plot_title, 
+                                  cmap, 
+                                  save=False, 
+                                  save_path=None):
     acc = get_accuracy(cm)
 
     cm = np.round(cm.astype('float') / cm.sum(axis=1)[:, np.newaxis], 2)
     mask = (cm == 0)
-    
-    fig, ax = plt.subplots(figsize=(8, 7), dpi=300)
+    sns.set_style('white')
+    fig, ax = plt.subplots(figsize=(10, 10), dpi=300)
     
     plt.rcParams.update({
         'font.family': 'sans-serif',
@@ -55,30 +65,35 @@ def _plot_pretty_confusion_matrix(cm, gestures, plot_title, cmap, save=False, sa
         xticklabels=gestures, 
         yticklabels=gestures, 
         cmap=cmap,
+        fmt='',
         linewidths=0.5,
+        linecolor='#DEDEDE',
         mask=mask,
+        cbar=True,
         cbar_kws={
-            "shrink": 0.8, 
-            "label": "Proportion", 
+            "shrink": 0.5, 
+            "label": "", 
             "drawedges": False,
             "ticks": [0, 0.25, 0.5, 0.75, 1.0]
         },
         annot_kws={
-            'weight': 'medium',  
-            'fontsize': 14
+            'weight': 'normal',  
+            'fontsize': 16
         }, 
         square=True
     )
     
-    ax.set_ylabel('Actual', fontsize=18)
-    ax.set_xlabel('Predicted', fontsize=18)
+    ax.set_ylabel('Actual', fontsize=22)
+    ax.set_xlabel('Predicted', fontsize=22)
     
-    ax.set_title(f'{plot_title}: {round(acc*100, 2)}%', fontsize=18, fontweight='bold') 
+    ax.set_title(f'{plot_title}: {round(acc*100, 2)}%', 
+                 fontsize=24, pad=20) 
     
     # Set ticks on both sides of axes
     ax.tick_params(axis='both', which='both', length=0)
-    ax.set_xticklabels(gestures, rotation=45, ha='right')
-    ax.set_yticklabels(gestures, rotation=0)
+    ax.set_xticklabels(gestures, rotation=45,
+                       ha='right', fontsize=18, wrap=True)
+    ax.set_yticklabels(gestures, rotation=0, fontsize=18, wrap=True)
 
     # Adjust layout
     plt.tight_layout()
