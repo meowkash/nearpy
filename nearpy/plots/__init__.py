203a60c87d9dae5ddc10b71064eb5c957ffcfe9a
Aakash Kapoor <113218196+meowkash@users.noreply.github.com>
2025-04-09 14:58:49 -0400

    Add code for viewing microphone segmentation

--- nearpy/plots/__init__.py
+++ nearpy/plots/__init__.py
@@ -3,3 +3,4 @@ from .timedomain import plot_routine_template, plot_time_series
 from .constants import BBC_THEME, DEFAULT_PLOT_STYLE
 from .projections import visualize_tsne
 from .summary import pretty_boxplot, pretty_scatterplot
+from .segment import plot_segmentation_results
--- nearpy/utils/__init__.py
+++ nearpy/utils/__init__.py
@@ -1,5 +1,5 @@
 from .accuracy import get_accuracy, get_class_accuracy
+from .benchmark import fn_timer
 from .files import tdms_to_csv, read_mat, read_tdms_v2, read_tdms
 from .logs import get_logger, log_print
 from .mimo import TxRx, get_mimo_channels, get_channels_from_df, split_channels
-from .benchmark import fn_timer
